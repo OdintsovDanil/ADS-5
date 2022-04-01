67b05654e415d8356f3f999669633182d29f8095
OdintsovDanil <99181188+OdintsovDanil@users.noreply.github.com>
2022-04-01 23:45:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,106 @@
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+int prior(char op) {
+    switch (op) {
+    case '(': return 0;
+    case ')': return 1;
+    case '-': return 2;
+    case '+': return 2;
+    case '*': return 3;
+    case '/': return 3;
+    case ' ': return -1;
+    default: return -2;
+    }
 }
 
-int eval(std::string pref) {
-  // добавьте код
-  return 0;
+int calc(int x, int y, char ch) {
+    switch (ch) {
+    case '+':
+        return y + x;
+    case '-':
+        return y - x;
+    case '*':
+        return y * x;
+    case '/':
+        return y / x;
+    default:
+        throw "Error while perform actions!";
+    }
+}
+
+string infx2pstfx(string inf) {
+    TStack<char, 100> st1;
+    string post = "";
+    int iter = 0;
+    while (inf.size() - 1 <= iter) {
+        if (prior(inf[iter]) == -2) {
+            if (iter < inf.size() && prior(inf[iter + 1]) == -2) {
+                while (iter < inf.size() && prior(inf[iter]) == -2) {
+                    post += inf[iter];
+                    iter++;
+                }
+                post += ' ';
+            }
+            else {
+                post += inf[iter];
+                post += ' ';
+            }
+            continue;
+        }
+        if (inf[iter] == '(' || prior(inf[iter]) > prior(st1.get()) || st1.isEmpty()) {
+            st1.push(inf[iter]);
+        }
+        else if (prior(inf[iter]) <= prior(st1.get())) {
+            while (prior(st1.get()) > 1) {
+                post += st1.get();
+                post += ' ';
+                st1.pop();
+            }
+            st1.push(inf[iter]);
+        }
+        if (inf[iter] == ')') {
+            while ((st1.get() != '(')) {
+                post += st1.get();
+                post += ' ';
+                st1.pop();
+            }
+            st1.pop();
+        }
+        iter++;
+    }
+    while (!st1.isEmpty()) {
+        if (prior(st1.get()) > 1) {
+            post += st1.get();
+            post += ' ';
+        }
+        st1.pop();
+    }
+    post.erase(post.size() - 1, 1);
+    return post;
+}
+
+int eval(string post) {
+    TStack<int, 100> st2;
+    int iter = 0;
+    for (int i = 0; i < post.size(); i++) {
+        iter = i;
+        string temp = "";
+        while (prior(post[iter]) == -2) {
+            temp += post[iter];
+            iter++;
+        }
+        i = iter;
+        if (prior(post[i]) > 1) {
+            int x = st2.get();
+            st2.pop();
+            int y = st2.get();
+            st2.pop();
+            st2.push(calc(x, y, post[i]));
+        }
+        if (temp != "") {
+            st2.push(stoi(temp));
+        }
+    }
+    return st2.get();
 }
